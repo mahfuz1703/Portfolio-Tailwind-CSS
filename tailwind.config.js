f39ef1a4f13ad0bd947ead97290705a1df3b57f8
Mahfuz Mia <sayhello.mahfuz@gmail.com>
2024-12-05 04:25:39 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ["./index.html", "./src/**/*.{html,js}"],
+    theme: {
+        fontFamily: {
+            manrope: ["Manrope", "sans-serif"],
+        },
+        extend: {},
+    },
+    plugins: [],
+}
